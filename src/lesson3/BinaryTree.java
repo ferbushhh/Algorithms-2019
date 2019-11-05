d13a8665110264d8de3ce663f69e9c62e280251b
ferbushhh <anna011299@yandex.ru>
2019-11-05 21:11:26 +0300

    third lesson

--- src/lesson3/BinaryTree.java
+++ src/lesson3/BinaryTree.java
@@ -71,23 +71,94 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
     /**
      * Удаление элемента в дереве
      * Средняя
+     *
+     * Оценка ресурсоемкости: R = O(1),
+     * Оценка трудоемкости: T = O(N)
      */
     @Override
     public boolean remove(Object o) {
-        /*T t = (T) o;
+        T t = (T) o;
         Node<T> node = find(t);
-        int value = (int) node.value;
 
-        if (node == null)
+        boolean isRightChild = false; //нужна для определения каким является удаляемый лист для родителя
+
+        if (node == null) //значит, что мы не нашли лист => удалять нечего
             return false;
 
-        if (node.left == null && node.right == null && node != root) {
-            
-        }*/
+        if (node == root && node.left == null && node.right == null) { //лист - корень и единственный элемент в дереве
+            root = null;
+            size--;
+            return true;
+        }
 
-        // TODO
-        throw new NotImplementedError();
+        Node<T> actual = root;
+        Node<T> parent = root;
 
+        while(!actual.equals(node)) { //в цикле определяем родителя для удаляемого листа и сам лист
+            parent = actual;
+            if (node.value.compareTo(actual.value) > 0) {
+                actual = actual.right;
+                isRightChild = true;
+            }
+            else {
+                actual = actual.left;
+                isRightChild = false;
+            }
+        }
+
+        if (actual.left == null && actual.right == null) { //если у лист нет потомков, то просто удаляем лист, ссылку у родителя меняем на null
+            if (isRightChild)
+                parent.right = null;
+            else
+                parent.left = null;
+        } else if (actual.left == null) { //если у листа только правый потомок - вместо удаляемого листа присваиваем родителю ссылку на потомка удаляемого
+            if (actual == root)
+                root = actual.right;
+            else if (isRightChild)
+                parent.right = actual.right;
+            else
+                parent.left = actual.right;
+        } else if (actual.right == null) { //аналогично
+            if (actual == root)
+                root = actual.left;
+            else if (isRightChild)
+                parent.right = actual.left;
+            else
+                parent.left = actual.left;
+        } else {
+            Node<T> follower = findFollower(actual); //находим того, кто его заменит
+
+            if (actual == root)
+                root = follower;
+            else if (isRightChild)
+                parent.right = follower;
+            else
+                parent.left = follower;
+
+            follower.left = actual.left;
+        }
+
+        size--;
+        return true;
+    }
+
+    private Node<T> findFollower(Node node) { //приемник ищется по алгоритму: по правой ветке самый левый лист
+        Node<T> parent = node;
+        Node<T> follower = node;
+        Node<T> actual = node.right;
+
+        while (actual != null) { //находим самый левый лист по правой ветке
+            parent = follower;
+            follower = actual;
+            actual = actual.left;
+        }
+
+        if (follower != node.right) {
+            parent.left = follower.right;
+            follower.right = node.right;
+        }
+
+        return follower;
     }
 
     @Override
@@ -120,38 +191,62 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
 
     public class BinaryTreeIterator implements Iterator<T> {
 
+        private int i = 0;
+        private ArrayList <Node<T>> listOfValues;
+
+        private Node<T> current = null;
+
         private BinaryTreeIterator() {
-            // Добавьте сюда инициализацию, если она необходима
+            listOfValues = new ArrayList<>();
+            bypass(root);
+        }
+
+        private void bypass(Node<T> nodeNew) { //обходим дерево, добавляем все в список
+            if(nodeNew != null) {
+                bypass(nodeNew.left);
+                listOfValues.add(nodeNew);
+                bypass(nodeNew.right);
+            }
         }
 
         /**
          * Проверка наличия следующего элемента
          * Средняя
+         *
+         * Оценка ресурсоемкости: R = O(1),
+         * Оценка трудоемкости: T = O(1)
          */
         @Override
         public boolean hasNext() {
-            // TODO
-            throw new NotImplementedError();
+            return i < listOfValues.size();
         }
 
         /**
          * Поиск следующего элемента
          * Средняя
+         *
+         * Оценка ресурсоемкости: R = O(n),
+         * Оценка трудоемкости: T = O(1)
          */
         @Override
         public T next() {
-            // TODO
-            throw new NotImplementedError();
+            current = listOfValues.get(i++);
+            if (current == null) return null;
+            else return current.value;
         }
 
         /**
          * Удаление следующего элемента
          * Сложная
+         *
+         * Оценка ресурсоемкости: R = O(1),
+         * Оценка трудоемкости: T = O(n)
          */
         @Override
         public void remove() {
-            // TODO
-            throw new NotImplementedError();
+            i--;
+            BinaryTree.this.remove(listOfValues.get(i).value);
+            listOfValues.remove(i);
         }
     }
 
@@ -194,6 +289,7 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
         // TODO
         throw new NotImplementedError();
     }
+
 
     /**
      * Найти множество всех элементов больше или равных заданного
--- test/lesson3/BinaryTreeTest.kt
+++ test/lesson3/BinaryTreeTest.kt
@@ -197,4 +197,5 @@ class BinaryTreeTest {
     fun testIteratorRemoveJava() {
         testIteratorRemove { createJavaTree() }
     }
+
 }
